6c57c6a12e8750b987fcbedca4b4efed58cd976c
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2024-06-06 12:46:14 -0500

    Create backing_up.sh

---
+++ backing_up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create a new folder in the $HOME directory with the current date as the name
+BACKUP_DIR="$HOME/$(date +%Y-%m-%d)"
+mkdir -p "$BACKUP_DIR"
+
+# Files and directories to back up
+CONFIG_FILE="$HOME/ceremonyclient/node/.config/config.yml"
+KEYS_FILE="$HOME/ceremonyclient/node/.config/keys.yml"
+STORE_DIR="$HOME/ceremonyclient/node/.config/store"
+
+# Copy files and directories to the backup directory
+cp "$CONFIG_FILE" "$BACKUP_DIR"
+cp "$KEYS_FILE" "$BACKUP_DIR"
+cp -r "$STORE_DIR" "$BACKUP_DIR"
